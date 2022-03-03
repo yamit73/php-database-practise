0dec07c02304abdb50bac70c897ab489ffae4b07
Amit Yadav <amityadav@cedcoss.com>
2022-03-03 11:21:36 +0530

    using order by statement

--- src/index.php
+++ src/index.php
@@ -23,7 +23,7 @@
    
     try {
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt = $conn->prepare("SELECT id, name, email FROM practise WHERE name='amit4'");
+        $stmt = $conn->prepare("SELECT id, name, email FROM practise ORDER BY id desc");
         $stmt->execute();
 
         // set the resulting array to associative
